f343e948adf82211efe427f78ef7b38cafea192c
adrianostevs <stevenadriano84@gmail.com>
2023-10-31 17:04:28 +0700

    Add network module and shared dependency module

---
+++ network/lib/network.dart
@@ -0,0 +1,9 @@
+library network;
+
+export 'package:network/network.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ shared_dependency/lib/shared_dependency.dart
@@ -0,0 +1,4 @@
+library shared_dependency;
+
+export 'package:dio/dio.dart';
+export 'package:hooks_riverpod/hooks_riverpod.dart';
